67521347bfbaba842e22df6f115dcb49e432780b
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-02-19 20:58:07 -0600

    file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import characters from "./Characters";
 
 class App extends Component {
+  state = {
+    characters: []
+  };
+
+  componentDidMount() {
+    this.setState({
+      characters
+    })
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="app">Hey!</div>;
   }
 }
 
 export default App;
+
+// 2 inputs - name imput and job input 
+// form - parent of inputs 
+// app --> render it all <FORM /> in app. 
+
+// Table component with 2 separate components that sync into it
+// table header 
+// table body 
---
+++ client/src/Characters.js
@@ -0,0 +1,17 @@
+const characters = [
+
+{name: 'Harry Potter', 
+job: 'chosen one'}, 
+
+{name: 'Dumbledore', 
+job: 'headmaster'},
+
+{name: 'Snape', 
+job: 'professor'}, 
+
+{name: 'Hermione', 
+job: 'student'}
+]
+
+
+export default characters 
